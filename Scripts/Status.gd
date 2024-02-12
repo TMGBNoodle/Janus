18c806e7c3680f1c0b69e862a6e86a3a3b3e7036
Neiiiko <113718017+Neiiiko@users.noreply.github.com>
2024-02-12 14:08:20 -0600

    Changes

--- Scripts/Status.gd
+++ Scripts/Status.gd
@@ -1,25 +1,51 @@
 extends Node2D
-var health = 200
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+@export var max_health : int = 200
+@export var timerLength = 1
+@export var regen : int = 1
+
+@onready var regenTween
+
+@export var health := max_health :
+	set(value):
+		health = set_health(value)
+	get:
+		return health
+		
+func set_health(__new_health : int):
+	return int(clamp(__new_health, 0, max_health))
+
+@onready var damage_timer : Timer = get_node("damageTimer")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func Regen():
+	regenTween = create_tween()
+	var regenTime = max_health - health / 2
+	regenTween.interpolate_value(health, max_health-health, 0, regenTime, 0, 0)
+	regenTween.play()
 
 func Damage(damage):
-	health -= damage
-	if health <= 0:
-		Destroy();
+	if regenTween:
+		regenTween.kill()
+	damage_timer.stop()
+	damage_timer.wait_time = timerLength
+	var new_health : int = int(clamp(health - damage, 0, max_health))
+	set_health(new_health)
+	damage_timer.start()
+	pass
+	
+func _ready():
+	damage_timer.timeout.connect(Regen)
+	Regen()
+	pass # Replace with function body.
 
-func Destroy():
-	print("Destroyed")
-	get_owner().Delete()
 
 func _on_hurtbox_area_entered(hitbox):
-	var baseDamage = hitbox.damage
-	Damage(baseDamage)
-	print("Enemy Hit new health", health)
+	var base_damage = hitbox.damage
+	self.Damage(hitbox.damage)
+	print("Enemy Hit")
